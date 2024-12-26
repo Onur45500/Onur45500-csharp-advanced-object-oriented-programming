5bba8b15d36ee97197e7e78df389c22b583cb368
Onur AKMESE <onur.akmese@viacesi.fr>
2024-12-26 16:02:28 +0100

    Displayed the values

--- Program.cs
+++ Program.cs
@@ -3,3 +3,10 @@ Rectangle rect2 = new Rectangle(30);
 
 Console.WriteLine(rect1.GetArea());
 Console.WriteLine(rect2.GetArea());
+
+rect1.BorderSize = 5;
+Console.WriteLine(rect1.BorderSize);
+
+rect2.Width = 5;
+rect2.Height = 10;
+Console.WriteLine(rect2.GetArea());
