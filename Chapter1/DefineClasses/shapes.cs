ea0a56162a1089f5ab6478aee1eab85b7c55a60d
Onur AKMESE <onur.akmese@viacesi.fr>
2024-12-26 15:47:55 +0100

    Added the make rectangle class

---
+++ Chapter1/DefineClasses/shapes.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Rectangle
+{
+    public Rectangle (int w, int h)
+    {
+        width = w;
+        height = h;
+    }
+
+    public Rectangle(int side)
+    {
+        width = height = side;
+    }
+
+    public int GetArea()
+    {
+        return width * height;
+    }
+
+    public int width;
+    public int height;
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Rectangle rect1 = new Rectangle(10, 20);
+Rectangle rect2 = new Rectangle(30);
+
+Console.WriteLine(rect1.GetArea());
+Console.WriteLine(rect2.GetArea());
